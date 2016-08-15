abd49ec81fc6d8dbb3c453aaf1f655e5d5a77d9a
Milan Conhye <milanconhye@gmail.com>
2016-08-15 18:53:25 +0100

    Copyright Info Updated

--- TCPIPClient/TCPIPClient/AboutBox.cs
+++ TCPIPClient/TCPIPClient/AboutBox.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 -------------------------------------------------
 |   Created by Milan Conhye                     |
 |   University of Greenwich                     |   
@@ -7,6 +7,22 @@
 |   GitHub: https://github.com/milanconhye      |
 |                                               |
 -------------------------------------------------
+
+Copyright (c) 2016 Milan Conhye
+
+* Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+* The software is provided "as is" and the author disclaims all warranties with regard 
+to this software including all implied warranties of merchantability and fitness. 
+This software, does not encrypt the information sent across the network.
+In no event shall the author be liable for any special, direct, indirect, or consequential 
+damages or any damages whatsoever resulting from loss of use, data or profits, whether in an 
+action of contract, negligence or other tortious action, arising out of or in connection with 
+the use or performance of this software. Please acknowledge and agree to this agreement 
+before using this software.
+
 */
 
 using System.Reflection;
